1794f6101c7a44140a36c6c1f81d6000470bfce1
Айгерим Сердалиева <aigerim.serdaliyeva@mail.ru>
2019-11-26 17:59:16 +0600

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <MainMenu />
+  </div>
+</template>
+
+<script>
+import MainMenu from './MainMenu.vue'
+
+export default {
+  components: {
+    MainMenu,
+
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ components/MainMenu.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul class="main-menu">
+    <li class="link">
+      <a href="">Наши преимещества</a>
+    </li>
+    <li class="link">
+      <a href="">Услуги</a>
+    </li>
+    <li class="link">
+      <a href="">Галерея</a>
+    </li>
+    <li class="link">
+      <a href="">Отзывы</a>
+    </li>
+    <li class="link">
+      <a href="">Контакты</a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
